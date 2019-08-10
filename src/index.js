59691a9c05930d34ab5c739c0df6e43083101aaf
Patrick Narciso <patrick.ncsilva@gmail.com>
2019-08-10 16:59:28 -0300

    feat(app): add styled normalize at the root of the project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Normalize } from 'styled-normalize';
 import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const Root = () => (
+  <>
+    <Normalize />
+    <App />
+  </>
+);
+
+ReactDOM.render(<Root />, document.getElementById('app'));
